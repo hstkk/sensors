1cdfd4d74a42207b88222100987e1d219789e980
Sami <hostikka@gmail.com>
2012-06-13 13:27:16 +0300

    Wifi test

--- droid/src/fi/hamk/DroidActivity.java
+++ droid/src/fi/hamk/DroidActivity.java
@@ -9,5 +9,7 @@ public class DroidActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        new Network(this).getWifi();
     }
 }
