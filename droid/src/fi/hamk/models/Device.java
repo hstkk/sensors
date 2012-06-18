8ac121adc2bc0ffaf06fd83b1ffa347a61687fbe
Sami <hostikka@gmail.com>
2012-06-18 10:23:54 +0300

    Removed unnecessary variables from device model

--- droid/src/fi/hamk/models/Device.java
+++ droid/src/fi/hamk/models/Device.java
@@ -7,19 +7,15 @@ import android.os.Build;
  */
 public class Device {
 
-	public String device;
 	public String manufacturer;
 	public String version;
 	public String brand;
 	public String model;
-	public String product;
 
 	public Device() {
-		this.device = Build.DEVICE;
 		this.manufacturer = Build.MANUFACTURER;
 		this.version = Build.VERSION.RELEASE;
 		this.brand = Build.BRAND;
 		this.model = Build.MODEL;
-		this.product = Build.PRODUCT;
 	}
 }
