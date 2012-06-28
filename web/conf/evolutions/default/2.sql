a4e419b5128144e7f5da86704215cd3eb80cc7b6
Sami <hostikka@gmail.com>
2012-06-28 12:55:34 +0300

    Evolutions: added more sample data

--- web/conf/evolutions/default/2.sql
+++ web/conf/evolutions/default/2.sql
@@ -1,7 +1,9 @@
 # --- !Ups
 
 insert into location values(1, 0, 62.7, 24.7);
+insert into location values(2, 0, 62, 24);
 insert into sensor (location_id) values(1);
+insert into sensor (location_id) values(2);
 
 # --- !Downs
 
