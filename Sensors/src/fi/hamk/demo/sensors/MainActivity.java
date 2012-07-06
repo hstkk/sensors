bd2d13084f58e85cc6e012bdb8612bf7248887c2
Sami <hostikka@gmail.com>
2012-07-06 12:36:17 +0300

    Android: ActionBar works!

--- Sensors/src/fi/hamk/demo/sensors/MainActivity.java
+++ Sensors/src/fi/hamk/demo/sensors/MainActivity.java
@@ -1,25 +1,56 @@
 package fi.hamk.demo.sensors;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.view.ViewPager;
-import com.viewpagerindicator.PageIndicator;
-import com.viewpagerindicator.TabPageIndicator;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.TextView;
 
-public class MainActivity extends FragmentActivity {
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+
+public class MainActivity extends SherlockActivity implements
+		ActionBar.TabListener {
+	final String[] TABS = { "network", "device", "location" };
+	TextView textView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		FragmentAdapter fragmentAdapter = new FragmentAdapter(
-				getSupportFragmentManager());
+		textView = (TextView) findViewById(R.id.textView);
 
-		ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
-		viewPager.setAdapter(fragmentAdapter);
+		// ActionBarSherlock settings
+		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-		PageIndicator pageIndicator = (TabPageIndicator) findViewById(R.id.tabPageIndicator);
-		pageIndicator.setViewPager(viewPager);
+		// ActionBarSherlock add tabs
+		for (String title : TABS) {
+			ActionBar.Tab tab = getSupportActionBar().newTab();
+			tab.setText(title);
+			tab.setTabListener(this);
+			getSupportActionBar().addTab(tab);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add("Refresh").setIcon(R.drawable.ic_refresh)
+				.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+		return true;
+	}
+
+	// @Override
+	public void onTabSelected(Tab tab, FragmentTransaction fragmentTransaction) {
+		textView.setText(tab.getText());
+	}
+
+	// @Override
+	public void onTabUnselected(Tab tab, FragmentTransaction fragmentTransaction) {
+	}
+
+	// @Override
+	public void onTabReselected(Tab tab, FragmentTransaction fragmentTransaction) {
 	}
 }
