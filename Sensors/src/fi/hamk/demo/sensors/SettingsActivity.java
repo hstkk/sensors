bcffb74bd0dd1e02ea867427bbf167bb53bf7c93
Sami <hostikka@gmail.com>
2012-07-09 14:25:23 +0300

    Android: Added ability to save and read settings

--- Sensors/src/fi/hamk/demo/sensors/SettingsActivity.java
+++ Sensors/src/fi/hamk/demo/sensors/SettingsActivity.java
@@ -1,5 +1,7 @@
 package fi.hamk.demo.sensors;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.webkit.URLUtil;
 import android.widget.CheckBox;
@@ -13,13 +15,20 @@ import com.actionbarsherlock.view.MenuItem;
 public class SettingsActivity extends SherlockActivity {
 	EditText url;
 	CheckBox shake;
+	SharedPreferences preferences;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_settings);
+
 		url = (EditText) findViewById(R.id.url);
 		shake = (CheckBox) findViewById(R.id.shake);
+		preferences = getSharedPreferences(getString(R.string.preferences), 0);
+		url.setText(preferences.getString(getString(R.string.preferences_url),
+				"http://127.0.0.1"));
+		shake.setChecked(preferences.getBoolean(
+				getString(R.string.preferences_shake), true));
 	}
 
 	@Override
@@ -32,9 +41,14 @@ public class SettingsActivity extends SherlockActivity {
 	@Override
 	public boolean onOptionsItemSelected(MenuItem menuItem) {
 		if (!URLUtil.isValidUrl(url.getText().toString()))
-			Toast.makeText(this, "Server is not valid", Toast.LENGTH_SHORT)
-					.show();
+			Toast.makeText(this, getString(R.string.save_err),
+					Toast.LENGTH_SHORT).show();
 		else {
+			Editor editor = preferences.edit();
+			editor.putBoolean(getString(R.string.preferences_shake),
+					shake.isChecked());
+			editor.putString(getString(R.string.preferences_url), url.getText()
+					.toString());
 			finish();
 		}
 		return true;
