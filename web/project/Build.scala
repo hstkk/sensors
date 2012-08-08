4ab760bceb3081c5f897430bca44ff041e49fd59
Sami <hostikka@gmail.com>
2012-08-08 10:25:01 +0300

    Add Heroku conf

--- web/project/Build.scala
+++ web/project/Build.scala
@@ -8,7 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-		"mysql" % "mysql-connector-java" % "5.1.18"
+		#"mysql" % "mysql-connector-java" % "5.1.18"
+		"postgresql"%"postgresql"%"9.1-901-1.jdbc4"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
