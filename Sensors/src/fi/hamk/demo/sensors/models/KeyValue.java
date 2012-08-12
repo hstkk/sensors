b9643f12e8d196ada07ab917dbd55d0708e23431
Sami <hostikka@gmail.com>
2012-08-12 17:06:57 +0300

    Added key value class

---
+++ Sensors/src/fi/hamk/demo/sensors/models/KeyValue.java
@@ -0,0 +1,13 @@
+package fi.hamk.demo.sensors.models;
+
+/**
+ * @author Sami Hostikka
+ */
+public class KeyValue {
+	public String key, value;
+
+	public KeyValue(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+}
